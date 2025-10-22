7a12eaaf3194c339aea344b9186c57edc9056dc6
JOAQUIN FRANKENTHAL <48801722@ort>
2025-10-22 08:34:42 -0300

    Formulario y Nuevo corregidos.

--- TP_FINAL_EFSI/src/pages/Nuevo.jsx
+++ TP_FINAL_EFSI/src/pages/Nuevo.jsx
@@ -2,17 +2,33 @@ import React from 'react'
 import MovimientoForm from '../components/MovimientoForm'
 import { useMovimientos } from '../context/MovimientosContext'
 
-function navigate(to){ window.history.pushState({}, '', to); window.dispatchEvent(new PopStateEvent('popstate')) }
+function navigate(to) {
+  window.history.pushState({}, '', to)
+  window.dispatchEvent(new PopStateEvent('popstate'))
+}
 
-export default function Nuevo(){
+export default function Nuevo() {
   const { agregarMovimiento } = useMovimientos()
-  const initial = { descripcion: '', categoria: '', tipo: '', monto: '', fecha: '' }
+
+  const initialValues = {
+    descripcion: '',
+    categoria: '',
+    tipo: '',
+    monto: '',
+    fecha: ''
+  }
 
   return (
     <div>
-      <h2>Nuevo movimiento</h2>
-      <MovimientoForm initialValues={initial} onSubmit={(vals)=>{ agregarMovimiento(vals); navigate('/') }} onCancel={()=>navigate('/')} />
+      <h2>Nuevo Movimiento</h2>
+      <MovimientoForm
+        initialValues={initialValues}
+        onSubmit={(vals) => {
+          agregarMovimiento(vals)
+          navigate('/')
+        }}
+        onCancel={() => navigate('/')}
+      />
     </div>
   )
 }
-
